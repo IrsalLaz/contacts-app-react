9d0c86a9c0efa7ad1a91b05b4e1dd5e9d954bc2e
Irsal Lazuard Izzatillah <irsallazuard547@gmail.com>
2024-11-19 13:32:39 +0700

    add and render components

---
+++ src/ContactApp.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ContactList from "./ContactList";
+import { getData } from "./data";
+
+function ContactApps(){
+    const contacts = getData();
+
+    return (
+        <div className="contact-app">
+            <h1>Contact App</h1>
+            <ContactList contacts={contacts} />
+        </div>
+    );
+}
+
+export default ContactApps;
--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-
-const element = <h1>Hello World!</h1>;
+import ContactApps from "./ContactApp";
 
 const root = createRoot(document.getElementById('root'));
 
-root.render(element);
+root.render(<ContactApps />);
