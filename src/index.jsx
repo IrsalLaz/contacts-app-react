252914546cca28cbcfdc4abf025a00e038c0bc96
Irsal Lazuard Izzatillah <irsallazuard547@gmail.com>
2024-11-19 13:41:32 +0700

    add css file as a module

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import ContactApps from "./ContactApp";
 
+// css style
+import "./style.css"
+
 const root = createRoot(document.getElementById('root'));
 
 root.render(<ContactApps />);
