44590bd1fcc03a1f3f68d2cb6a12a080bc8090a4
Irsal Lazuard Izzatillah <irsallazuard547@gmail.com>
2025-01-18 21:16:21 +0700

    refactor: split app into pages and components and use router
    use router for homePage and addPage

--- src/components/ContactApp.jsx
+++ src/components/ContactApp.jsx
@@ -1,55 +1,26 @@
 import React from "react";
 import ContactList from "./ContactList";
 import ContactInput from "./ContactInput";
-import { getData } from "../utils/data";
+import Navigation from "./Navigation";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "../pages/HomePage";
+import AddPage from "../pages/AddPage";
 
-class ContactApps extends React.Component {
-	constructor(props) {
-		super(props);
-
-		this.state = {
-			contacts: getData(),
-		};
-
-		this.onDeleteHandler = this.onDeleteHandler.bind(this);
-		this.onAddContactHandler = this.onAddContactHandler.bind(this);
-	}
-
-	onDeleteHandler(id) {
-		const contacts = this.state.contacts.filter((contact) => contact.id != id);
-		this.setState({ contacts });
-	}
-
-	onAddContactHandler({ name, tag }) {
-		this.setState((prevState) => {
-			return {
-				contacts: [
-					...prevState.contacts,
-					{
-						id: +new Date(),
-						name,
-						tag,
-						imageUrl: "/images/default.jpg",
-					},
-				],
-			};
-		});
-	}
-
-	render() {
-		return (
-			<div className="contact-app">
+function ContactApps() {
+	return (
+		<div className="contact-app">
+			<header className="contact-app__header">
 				<h1>Contact App</h1>
-				<h2>Tambah Kontak</h2>
-				<ContactInput addContact={this.onAddContactHandler} />
-
-				<ContactList
-					contacts={this.state.contacts}
-					onDelete={this.onDeleteHandler}
-				/>
-			</div>
-		);
-	}
+				<Navigation />
+			</header>
+			<main>
+				<Routes>
+					<Route path="/" element={<HomePage />} />
+					<Route path="/add" element={<AddPage />} />
+				</Routes>
+			</main>
+		</div>
+	);
 }
 
 export default ContactApps;
--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import ContactApps from "./components/ContactApp";
+import { BrowserRouter } from "react-router-dom";
 
 // css style
-import "./styles/style.css"
+import "./styles/style.css";
 
-const root = createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById("root"));
 
-root.render(<ContactApps />);
+root.render(
+	<BrowserRouter>
+		<ContactApps />
+	</BrowserRouter>
+);
