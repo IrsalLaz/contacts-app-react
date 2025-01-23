ecd8f08dc7fb92c54d98a37c6562881996e9a55d
Irsal Lazuard Izzatillah <irsallazuard547@gmail.com>
2025-01-23 12:22:30 +0700

    refactor: change contact app to class component

--- src/components/ContactApp.jsx
+++ src/components/ContactApp.jsx
@@ -3,22 +3,50 @@ import Navigation from "./Navigation";
 import { Route, Routes } from "react-router-dom";
 import HomePage from "../pages/HomePage";
 import AddPage from "../pages/AddPage";
+import RegisterPage from "../pages/RegisterPage";
 
-function ContactApps() {
-	return (
-		<div className="contact-app">
-			<header className="contact-app__header">
-				<h1>Contact App</h1>
-				<Navigation />
-			</header>
-			<main>
-				<Routes>
-					<Route path="/" element={<HomePage />} />
-					<Route path="/add" element={<AddPage />} />
-				</Routes>
-			</main>
-		</div>
-	);
+class ContactApps extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			authedUser: null,
+		};
+	}
+
+	render() {
+		if (this.state.authedUser === null) {
+			return (
+				<div className="contact-app">
+					<header className="contact-app__header">
+						<h1>Contact App</h1>
+					</header>
+
+					<main>
+						<Routes>
+							<Route path="/*" element={<p>Halaman Login</p>} />
+							<Route path="/register" element={<RegisterPage />} />
+						</Routes>
+					</main>
+				</div>
+			);
+		}
+
+		return (
+			<div className="contact-app">
+				<header className="contact-app__header">
+					<h1>Contact App</h1>
+					<Navigation />
+				</header>
+				<main>
+					<Routes>
+						<Route path="/" element={<HomePage />} />
+						<Route path="/add" element={<AddPage />} />
+					</Routes>
+				</main>
+			</div>
+		);
+	}
 }
 
 export default ContactApps;
